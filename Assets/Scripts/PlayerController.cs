af880eba519f898e5416cc428cdee71300879d10
NewestAF <castle5314@naver.com>
2022-02-25 23:59:53 +0900

    feat: Add Jumping Function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,23 +10,30 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float crouchSpeed;
+    [SerializeField] private float crouchPosY;
 
     #endregion
 
     #region Condition
 
-    private bool isRunning;
+    private bool isRunning = false;
+    private bool isGround = true;
+    
 
     #endregion
     
 
     private new Rigidbody rigidbody;
+    private CapsuleCollider capsuleCollider;
     
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
     void Awake()
     {
@@ -35,13 +43,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckIsGround();
+        TryJump();
         Move();
         RotateCharacter();
     }
 
     private void Move()
     {
+        
         CheckIsRunning();
+
+        float actualSpeed = isRunning ? runSpeed : walkSpeed;
         
         float moveDirX = Input.GetAxisRaw("Horizontal");
         float moveDirZ = Input.GetAxisRaw("Vertical");
@@ -51,7 +64,7 @@ public class PlayerController : MonoBehaviour
         Vector3 moveHorizontal = trans.right * moveDirX;
         Vector3 moveVertical = trans.forward * moveDirZ;
 
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * actualSpeed;
 
         rigidbody.MovePosition(trans.position + velocity * Time.deltaTime);
 
@@ -61,9 +74,31 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
         }
         
+    }
+
+    private void CheckIsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            DoJump();
+        }
+    }
+
+    private void DoJump()
+    {
+        rigidbody.velocity = transform.up * jumpForce;
     }
 
     private void RotateCharacter()
